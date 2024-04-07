22ea12238bb80387c2fce6fb282a142f38e4bcd9
msl <340541293@qq.com>
2024-04-07 22:30:31 +0800

    LC-Q66  simple

---
+++ src/leetcode/classics150/Q66.java
@@ -0,0 +1,26 @@
+package leetcode.classics150;
+
+public class Q66 {
+    public static void main(String[] args) {
+        int[] digits = {9, 9, 9, 9, 9, 9};
+        int[] result = plusOne(digits);
+        for (int i : result){
+            System.out.println(i);
+        }
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if (digits[i] != 0)
+                return digits;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+}
+
+
