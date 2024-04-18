0d863b1355d1dbfe6e7bfdf027397602f7ed6526
ma_shaolong <ma_shaolong@topsec.com.cn>
2024-04-18 10:06:58 +0800

    test

--- src/leetcode/Test.java
+++ src/leetcode/Test.java
@@ -2,6 +2,11 @@ package leetcode;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE);
+        int[] digits = {9, 8, 9, 9, 9, 9};
+        digits = new int[digits.length + 1];
+        digits[0] = 7;
+        for (int i : digits) {
+            System.out.println(i);
+        }
     }
 }
